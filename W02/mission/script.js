cc5b25dac3ee330c897dd42226b4d196b9c43db4
kimjosell <kimjosellag@gmail.com>
2026-09-26 11:20:10 -0600

    feat: update mission page with subtitle styling and theme color changes

--- W02/mission/script.js
+++ W02/mission/script.js
@@ -7,12 +7,15 @@ selectElem.addEventListener('change', changeTheme);
 function changeTheme() {
     let current = selectElem.value;
      body = document.querySelector(".change_mode");
+     h2 = document.querySelector("h2");
     if (current == 'dark') {
         body.classList.add("dark_mode");
         logo.src = "byui-logo-black.png";
+        h2.style.color = "#94cdf3";
     } else {
         body.classList.remove("dark_mode");
         logo.src = "byui-logo-blue.webp";
+        h2.style.color = "#006EB6";
     }
 }           
                     
